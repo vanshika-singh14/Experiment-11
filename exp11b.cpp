67e7db72f853add289fcda49739b097fa4cd8fb5
vanshika-singh14 <vanshikajksingh1405@gmail.com>
2024-10-06 17:33:07 +0530

    Create exp11b.cpp

---
+++ exp11b.cpp
@@ -0,0 +1,22 @@
+//Vanshika Singh
+//23070123148
+
+#include<iostream>
+using namespace std;
+class cuboid
+{
+    public:
+    double h = 5.0,b = 3.0,l = 8.0;
+    double volume()
+    {
+        double vol = h * b * l;
+        cout<<"Volume "<<vol<<endl;
+    }
+
+};
+int main()
+{
+    cuboid c1;
+    c1.volume();
+  
+}
